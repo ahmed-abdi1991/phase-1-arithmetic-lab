fb444b654c73fdc21777facbad02d5144e1355e1
ahmed-abdi1991 <squarehajji@gmail.com>
2023-09-23 22:45:12 +0300

    assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1=2
+let num2=31
+ const multiply=num1*num2
 
-// Write your code here
+const random=Math.floor(Math.random())+1
+
+let num3=30
+let num4=13
+const mod=num3%num4
+
+const max=Math.max(10,20,15,3,9)
